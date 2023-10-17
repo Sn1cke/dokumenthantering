1d4fff4b677a82b5d760efbe6fce09185bf03976
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-10-17 12:28:04 +0200

    add theme and base layout

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="navbar bg-neutral text-neutral-content">
+        <a className="btn btn-ghost normal-case text-xl">Dokke</a>
+      </div>
+    </header>
+  );
+}
